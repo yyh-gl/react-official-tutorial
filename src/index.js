5ea0e535a9f809935d2b3eca738cea59df2e8fda
yyh-gl <yhonda.95.gl@gmail.com>
2019-05-08 22:57:26 +0900

    Squareから不要なstateを削除

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
       <button className="square" onClick={() => this.props.onClick()}>
